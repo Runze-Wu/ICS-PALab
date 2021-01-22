721c51ec9451344c38ea85671b70ebce36c57f08
wrz <wrznull@outlook.com>
2021-01-22 17:04:10 +0800

    pa1 cpu modified done

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -18,29 +18,32 @@
  */
 
 typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
+	union {
+		union {
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
 
-  /* Do NOT change the order of the GPRs' definitions. */
+		/* Do NOT change the order of the GPRs' definitions. */
 
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
-   * in PA2 able to directly access these registers.
-   */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
-  vaddr_t pc;
+		/* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+		 * in PA2 able to directly access these registers.
+		 */
+		struct {	
+			rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
+	};
+	vaddr_t pc;
 } x86_CPU_state;
 
 // decode
 typedef struct {
-  bool is_operand_size_16;
-  uint8_t ext_opcode;
-  const rtlreg_t *mbase;
-  rtlreg_t mbr;
-  word_t moff;
+	bool is_operand_size_16;
+	uint8_t ext_opcode;
+	const rtlreg_t *mbase;
+	rtlreg_t mbr;
+	word_t moff;
 } x86_ISADecodeInfo;
 
 #define suffix_char(width) ((width) == 4 ? 'l' : ((width) == 1 ? 'b' : ((width) == 2 ? 'w' : '?')))
